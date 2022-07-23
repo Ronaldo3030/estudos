64d778cf35bf22c9c5391adb14b0249b57867ef6
ronaldo3030 <ronaldomacielcamposjunior@gmail.com>
2022-07-23 19:45:29 -0300

    nomeando rotas

--- PHP LARAVEL/laravel1/routes/web.php
+++ PHP LARAVEL/laravel1/routes/web.php
@@ -13,20 +13,33 @@ use Illuminate\Support\Facades\Route;
 Route::view('/', 'welcome');
 Route::view('/teste', 'teste');
 
+
+
 // ROTA DINAMICA PASSADA PELA URL
 Route::get('/noticia/{slug}', function ($slug) {
     echo "Slug: " . $slug;
 });
-
 Route::get('/noticia/{slug}/comentario/{id}', function ($slug, $id) {
     echo "Slug: " . $slug . "<br>";
     echo "Comentario: " . $id;
 });
-
 Route::get('/user/{id}', function ($id) {
     echo "Usuario ID: " . $id;
 });
-
 Route::get('/user/{name}', function ($name) {
     echo "Usuario nome: " . $name;
 })->where('name', '[a-z]+');
+
+
+
+Route::get('/config', function () {
+    return redirect()->route('info');
+
+    return view('config');
+});
+Route::get('/config/info', function () {
+    echo "Configurações INFO.";
+})->name('info');
+Route::get('/config/permissoes', function () {
+    echo "Configurações PERMISSÕES.";
+})->name('permissoes');
