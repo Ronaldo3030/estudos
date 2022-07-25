4bbd1d837a1568fe226e0ba0e991df98c3a916dd
ronaldo3030 <ronaldomacielcamposjunior@gmail.com>
2022-07-25 17:15:56 -0300

    request de paramentros laravel

--- PHP LARAVEL/laravel1/app/Http/Controllers/ConfigController.php
+++ PHP LARAVEL/laravel1/app/Http/Controllers/ConfigController.php
@@ -6,7 +6,18 @@ use Illuminate\Http\Request;
 
 class ConfigController extends Controller
 {
-    public function index(){
+    public function index(Request $req){
+        // all, input, query
+        // ALL pega todos os dados
+        // INPUT prioriza dados do corpo, se n tiver pega da url
+        // QUERY pega os dados da url
+
+        // $data = $req->all();
+
+        // $data = $req->input('nome');
+        $data = $req->query('nome', 'Visitante');
+
+        print_r($data);
         return view('config');
     }
     public function info(){
--- PHP LARAVEL/laravel1/resources/views/config.blade.php
+++ PHP LARAVEL/laravel1/resources/views/config.blade.php
@@ -1,3 +1,11 @@
 <h1>Configurações</h1>
 
+<form method="GET">
+    Nome:<br>
+    <input type="text" name="nome"><br>
+    Idade:<br>
+    <input type="text" name="idade"><br>
+    <input type="submit" value="Enviar">
+</form>
+
 ...
