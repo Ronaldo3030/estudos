2cd7a3ddb1769b1c3efe3ae12339f387e26d9232
ronaldo3030 <ronaldomacielcamposjunior@gmail.com>
2022-07-27 11:20:51 -0300

    iniciado projeto

---
+++ projeto-aposta/projeto-aposta/resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.padrao')
+
+@section('title', 'Home')
+    
+@section('content')
+    <h1>Bem vindo</h1>
+@endsection
---
+++ projeto-aposta/projeto-aposta/resources/views/layouts/padrao.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <style type="text/css">
+        @yield('style');
+    </style>
+
+    <title>Bet your luck - @yield('title')</title>
+</head>
+
+<body>
+    <header class="navbar navbar-dark bg-dark px-5 py-2" style="color: white;">
+        <h3>icon</h3>
+        <div>
+            <button>Registre-se</button>
+            <button>Login</button>
+        </div>
+    </header>
+
+    <section>
+        @yield('content')
+    </section>
+
+    <footer>footer</footer>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
+        integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
+        integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
