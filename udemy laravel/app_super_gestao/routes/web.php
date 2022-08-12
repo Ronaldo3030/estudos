99a28a1dbeaac4078e8707050711a8f0c2a18c8f
ronaldo3030 <ronaldomacielcamposjunior@gmail.com>
2022-08-12 15:00:21 -0300

    adicionado middleware

--- udemy laravel/app_super_gestao/routes/web.php
+++ udemy laravel/app_super_gestao/routes/web.php
@@ -7,9 +7,12 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', 'App\Http\Controllers\HomeController');
+Route::get('/login', function(){
+    echo "Página login";
+})->name('login');
 
 Route::prefix('/tarefas')->group(function(){
-    Route::get('/', 'App\Http\Controllers\TarefasController@list')->name('tarefas.list'); //listagem de tarefas
+    Route::get('/', 'App\Http\Controllers\TarefasController@list')->name('tarefas.list')->middleware('auth'); //listagem de tarefas
 
     Route::get('/add', 'App\Http\Controllers\TarefasController@add')->name('tarefas.add'); //adição de tarefas
     Route::post('/add', 'App\Http\Controllers\TarefasController@addAction'); //ação de adição de tarefas
@@ -21,3 +24,14 @@ Route::prefix('/tarefas')->group(function(){
 
     Route::get('/marcar/{id}', 'App\Http\Controllers\TarefasController@done')->name('tarefas.done'); //marcar resolvido/não
 });
+
+Route::resource('todo', 'App\Http\Controllers\TodoController');
+/*
+GET -> /todo - index - NOME: todo.index - LISTA OS ITENS
+GET -> /todo/create - create - NOME: todo.create - FORM DE CRIAÇÃO DE ITENS
+POST -> /todo - store - NOME: todo.store - RECEBER OS DADOS E ADD ITEM (add action)
+GET -> /todo/{id} - show - NOME: todo.show - ITEM INDIVIDUAL
+GET -> /todo/{id}/edit - edit - NOME: todo.edit - FORM DE EDIÇÃO DE ITENS
+PUT -> /todo/{id} - update - NOME: todo.update - RECEBE OS DADOS E UPDATE ITEN (edit action)
+DELETE -> /todo/{id} - destroy - NOME: todo.destroy - DELETA O ITEM
+*/
