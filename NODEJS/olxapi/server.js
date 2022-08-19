486421b1f477a3705f3156b5e9d7ec52332d138a
ronaldo3030 <ronaldomacielcamposjunior@gmail.com>
2022-08-19 20:27:29 -0300

    criado controllers

--- NODEJS/olxapi/server.js
+++ NODEJS/olxapi/server.js
@@ -3,6 +3,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const fileupload = require('express-fileupload');
+
+const apiRoutes = require('./src/routes');
+
 mongoose.connect(process.env.DATABASE, {
     useNewUrlParser: true,
     // useFindAndModify: false,
@@ -22,9 +25,7 @@ server.use(fileupload());
 
 server.use(express.static(__dirname + '/public'));
 
-server.get('/ping', (req, res) => {
-    res.json({pong: true});
-});
+server.use('/', apiRoutes);
 
 server.listen(process.env.PORT, () => {
     console.log("Servidor rodando em: "+ process.env.BASE);
---
+++ NODEJS/olxapi/src/controllers/AdsController.js
@@ -0,0 +1,19 @@
+const { editAction } = require("./UserController");
+
+module.exports = {
+    getCategories: async (req, res) => {
+        
+    },
+    addAction: async (req, res) => {
+        
+    },
+    getList: async (req, res) => {
+        
+    },
+    getItem: async (req, res) => {
+        
+    },
+    editAction: async (req, res) => {
+        
+    }
+};
