515fdafc6ce58f9e3c70c21b8de9e6adde69e228
ronaldo3030 <ronaldomacielcamposjunior@gmail.com>
2022-08-22 10:44:49 -0300

    add rota getstates

--- NODEJS/olxapi/src/controllers/UserController.js
+++ NODEJS/olxapi/src/controllers/UserController.js
@@ -1,6 +1,11 @@
+const State = require('../models/State');
+
 module.exports = {
     getStates: async (req, res) => {
-        
+        let states = await State.find(); // SELECT
+        res.json({
+            states: states
+        });
     },
 
     info: async (req, res) => {
