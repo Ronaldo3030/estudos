6e94da92798d4a77abfa6bd0857d02f996da1f07
Ronaldo3030 <ronaldomacielcamposjunior@gmail.com>
2022-09-01 01:17:22 -0300

    adicionado rota de statement

--- NODEJS/Rocket/projeto1/src/index.js
+++ NODEJS/Rocket/projeto1/src/index.js
@@ -29,4 +29,16 @@ app.post('/account', (req, res) => {
     res.status(201).send();
 });
 
+app.get('/statement/:cpf', (req, res) => {
+    const { cpf } = req.params;
+
+    const customer = customers.find(customer => customer.cpf === cpf);
+
+    if(!customer){
+        return res.status(400).json({error: "Cpf not exist!"});
+    }
+
+    return res.json(customer.statement);
+});
+
 app.listen(3333);
