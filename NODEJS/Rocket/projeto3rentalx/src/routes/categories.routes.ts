46164b2057bfdb1b68501470d06571665536262d
Ronaldo3030 <ronaldomacielcamposjunior@gmail.com>
2022-09-02 15:08:21 -0300

    adicionado rota que retorna todas categorias

--- NODEJS/Rocket/projeto3rentalx/src/routes/categories.routes.ts
+++ NODEJS/Rocket/projeto3rentalx/src/routes/categories.routes.ts
@@ -7,9 +7,21 @@ const categoriesRepository = new CategoriesRepository;
 categoriesRoutes.post("/", (req, res) => {
     const { name, description } = req.body;
 
-    categoriesRepository.create({name, description});
+    categoriesRepository.create({ name, description });
 
     return res.status(201).send();
 });
+
+categoriesRoutes.get("/", (req, res) => {
+    const all = categoriesRepository.list();
+
+    const categoriesExist = all.some((item) => item);
+
+    if(!categoriesExist){
+        return res.status(400).json({error: "Nothing categories"});
+    }
+
+    return res.json(all);
+})
 
 export { categoriesRoutes };
