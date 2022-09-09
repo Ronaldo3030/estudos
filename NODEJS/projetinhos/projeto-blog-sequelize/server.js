954bb7e4808a1d4fa8b54c3e2d7be75678ab4602
Ronaldo3030 <ronaldomacielcamposjunior@gmail.com>
2022-09-09 14:02:49 -0300

    add novo projeto blog

---
+++ NODEJS/projetinhos/projeto-blog-sequelize/server.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const routes = require('./src/routes');
+const port = process.env.PORT || '3000';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use('/', routes);
+
+app.listen(port, () => {
+    console.log("Servidor rodando na porta: " + port);
+});
