52847c709075282a3ef7952ab50e279f624a87d3
Ronaldo3030 <ronaldomacielcamposjunior@gmail.com>
2022-09-11 15:16:38 -0300

    adicionado variaveis do bd

--- NODEJS/projetinhos/projeto-blog-sequelize/src/db.js
+++ NODEJS/projetinhos/projeto-blog-sequelize/src/db.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('bd_blog', process.env.USER, process.env.PASS, {
+const sequelize = new Sequelize(process.env.DB, process.env.USER, process.env.PASS, {
     dialect: 'mysql',
     host: process.env.HOST,
     port: process.env.PORT_DB
