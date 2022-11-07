bc2096cdd5f76023def13a0f2642d4ba3343fb65
Ronaldo3030 <ronaldomacielcamposjunior@gmail.com>
2022-11-07 09:20:59 -0300

    finalizado api mais clean

--- CURSO ALURA/nodejs/primeiro-curso/src/app.js
+++ CURSO ALURA/nodejs/primeiro-curso/src/app.js
@@ -14,26 +14,4 @@ app.use(express.json());
 
 routes(app);
 
-app.get('/livros/:id', (req, res) => {
-  let index = searchLivro(req.params.id);
-  res.status(200).json(livros[index]);
-});
-
-app.put('/livros/:id', (req, res) => {
-  let index = searchLivro(req.params.id);
-  livros[index].titulo = req.body.titulo;
-  res.status(200).json(livros);
-});
-
-app.delete('/livros/:id', (req, res) => {
-  let {id} = req.params;
-  let index = searchLivro(id);
-  livros.splice(index, 1);
-  res.send(`Livro ${id} removido com sucesso!`);
-});
-
-function searchLivro(id){
-  return livros.findIndex(livro => livro.id == id);
-}
-
 export default app;
--- CURSO ALURA/nodejs/primeiro-curso/src/controllers/livrosController.js
+++ CURSO ALURA/nodejs/primeiro-curso/src/controllers/livrosController.js
@@ -11,10 +11,46 @@ class LivroController {
   static cadastrarLivro = (req, res) => {
     let livro = new livros(req.body);
     livro.save((err) => {
-      if(err) {
+      if (err) {
         res.status(500).send({ message: `${err.message} - falha ao cadastrar livro!` });
       } else {
         res.status(201).send(livro.toJSON());
+      }
+    });
+  }
+
+  static atualizarLivro = (req, res) => {
+    const id = req.params.id;
+
+    livros.findByIdAndUpdate(id, { $set: req.body }, (err) => {
+      if (!err) {
+        res.status(200).send({ message: "Livro atualizado com sucesso!" });
+      } else {
+        res.status(500).send({ message: err.message });
+      }
+    });
+  }
+
+  static listarLivroPorId = (req, res) => {
+    const id = req.params.id;
+
+    livros.findById(id, (err, livros) => {
+      if (err) {
+        res.status(400).send({ message: `${err.message} - ID do livro não identificada!` });
+      } else {
+        res.status(200).send(livros);
+      }
+    });
+  }
+
+  static excluirLivro = (req, res) => {
+    const id = req.params.id;
+
+    livros.findByIdAndRemove(id, (err) => {
+      if (!err) {
+        res.status(200).send({ message: "Livro removido com sucesso!" });
+      } else {
+        res.status(500).send({ message: err.message });
       }
     });
   }
